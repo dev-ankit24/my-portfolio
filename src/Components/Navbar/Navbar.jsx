841738711b4c11a63b812220c03bc06b3a76e95e
dev-ankit24 <ankitkumar839282@gmail.com>
2024-08-19 10:51:42 +0530

    create navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Navbar.css"
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <img src="" alt="" />
+        <ul className="nav-menu">
+            <li>Home</li>
+            <li>About Me</li>
+            <li>Services</li>
+            <li>Portfolio</li>
+            <li>Contact</li>
+        </ul>
+        
+
+    </div>
+  )
+}
+
+export default Navbar
