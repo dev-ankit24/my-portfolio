302b5de0e686443bf7da102cc2cf3da6f9a69296
dev-ankit24 <ankitkumar839282@gmail.com>
2024-08-20 11:04:40 +0530

    create anbavr

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import "./Navbar.css"
+import logo1 from '../../assets/logo1.png'
 
 function Navbar() {
   return (
     <div className='navbar'>
-        <img src="" alt="" />
+        <img className='nav-logo' src={logo1} alt="" />
         <ul className="nav-menu">
             <li>Home</li>
             <li>About Me</li>
@@ -12,8 +13,8 @@ function Navbar() {
             <li>Portfolio</li>
             <li>Contact</li>
         </ul>
-        
 
+<div className="nav-connect">Connect With Me</div>
     </div>
   )
 }
